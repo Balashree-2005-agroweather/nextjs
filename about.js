84845f9f81a254c918ef011f475e9ba4043fe8af
Balashree-2005-agroweather <dbalashree@gmail.com>
2025-06-16 16:30:56 +0530

    Create about.js

---
+++ about.js
@@ -0,0 +1,8 @@
+export default function About() {
+  return (
+    <div>
+      <h1>About Weather Prediction App</h1>
+      <p>This app provides real-time weather predictions based on location.</p>
+    </div>
+  );
+}
